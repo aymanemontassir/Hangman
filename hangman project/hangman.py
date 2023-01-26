658960b7a123cfd6819923d49ceef7d9d2cb9c7b
aymanemontassir <aymane-hana@hotmail.fr>
2023-01-26 18:23:49 +0100

    adding check the guess word function

--- hangman project/hangman.py
+++ hangman project/hangman.py
@@ -1,4 +1,15 @@
+def check_word(word,answer):
+    if word == answer:
+        return True
+    else:
+        return False
+    
 def print_menu():
     print("H A N G M A N\nThe game will be available soon.")
-
+# -------------------------------------
 print_menu()
+x = input("Guess the word:") 
+if check_word(x,"python") == True:
+    print("You survived!")
+else:
+    print("You lost!") 
