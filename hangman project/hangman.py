edc8d63256fb13aeac3d56131faf1142b9b04559
aymanemontassir <aymane-hana@hotmail.fr>
2023-01-26 19:40:52 +0100

    iterating through a list of words

--- hangman project/hangman.py
+++ hangman project/hangman.py
@@ -1,3 +1,4 @@
+import random as rnd
 def check_word(word,answer):
     if word == answer:
         return True
@@ -8,8 +9,12 @@ def print_menu():
     print("H A N G M A N\nThe game will be available soon.")
 # -------------------------------------
 print_menu()
-x = input("Guess the word:") 
-if check_word(x,"python") == True:
+words = ('python',"java","swift","javascript")
+i = round(rnd.uniform(0,3))
+answer = words[i]
+
+word = input("Guess the word:") 
+if check_word(word,answer) == True:
     print("You survived!")
 else:
     print("You lost!") 
